b33d8fabdcd61c9fd049483dbdce5aca665e770e
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2022-05-13 10:50:27 +0200

    qsdfhjklm

--- src/model/Donnee/Chouette.java
+++ src/model/Donnee/Chouette.java
@@ -10,13 +10,46 @@ public class Chouette implements IObs<ObsChouette> {
     private String idChouette;
 
     /**
-     * 
-     * @param id
-     * @param leSexe
-     * @param IEspece
+     * Constructor of Chouette
+     * @param id id of this Chouette
+     * @param leSexe sexe of this Chouette
+     * @param IEspece espece of this Chouette
      */
     public Chouette(String id, Sexe leSexe, EspeceChouette IEspece) {
-        // TODO - implement Chouette.Chouette
-        throw new UnsupportedOperationException();
+        if (id != null) {
+            this.idChouette = id;
+            this.sexe = leSexe;
+            this.espece = IEspece;
+        }
+    }
+
+    @Override
+    public void ajouteUneObs(ObsChouette obs) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ajoutePlsObs(ArrayList<ObsChouette> obs) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void videObs() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean retireObs(int idObs) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int nbObs() {
+        // TODO Auto-generated method stub
+        return 0;
     }
 }
--- src/model/Donnee/ObsChouette.java
+++ src/model/Donnee/ObsChouette.java
@@ -5,16 +5,22 @@ public class ObsChouette extends Observation {
     private TypeObservation typeObs;
 
     /**
-     * 
-     * @param id
-     * @param date
-     * @param heure
-     * @param lieu
-     * @param observateurs
+     * Constructor of ObsChouette
+     * @param id id of Observation
+     * @param date date of Observation
+     * @param heure hours of Observation
+     * @param lieu place of Observation
+     * @param observateurs liste of Observateurs for this Observation
+     * @param type typeObservation
      */
-    public ObsChouette(int id, Date date, Time heure, Lieu lieu, ArrayList<Observateur> observateurs) {
-        // TODO - implement ObsChouette.ObsChouette
-        throw new UnsupportedOperationException();
+    public ObsChouette(int id, Date date, Time heure, Lieu lieu, ArrayList<Observateur> observateurs, TypeObservation type) {
+        super(id, date, heure, lieu, observateurs);
+        this.typeObs = type;
+    }
+
+    @Override
+    public EspeceObservee especeObs() {
+        return EspeceObservee.CHOUETTE;
     }
 
 }
