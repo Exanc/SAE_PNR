2207b7572409816d22630f4a7731d101abf2dee9
Azukii <nolannbonhoure@hotmail.com>
2022-05-16 12:44:10 +0200

    OK

--- src/modele/traitement/DataBatricien.java
+++ src/modele/traitement/DataBatricien.java
@@ -3,7 +3,6 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import donnee.*;
 
 public class DataBatricien {
 
@@ -17,18 +16,27 @@ public class DataBatricien {
     public void getAll () {
         this.c = setC("jdbc:mysql://localhost:3306/myDB");
 
-        Statement statement = this.c.getStatement();
+        Statement statement = this.c.createStatement();
 
-        ResultSet rs = statement.executeQuery("SELECT NOM,PRENOM FROM OBS_BATRACIEN ");
+        ResultSet rs = statement.executeQuery("SELECT * FROM OBS_BATRACIEN ");
 
-        int id = 0;
         while (rs.next()) {
-            String nom = rs.getString("NOM");
-            String prenom = rs.getString("PRENOM");
-            for (ObsBatracien obsBatracien : liste_ObsBatricien) {
-                obsBatracien = new ObsBatracien(id);
-                id++;
+            String id = rs.getString("ID");
+            String date = rs.getString("DATE");
+            String heure = rs.getString("HEURE");
+            String lieu = rs.getString("LIEU");
+            String observateurs = rs.getString("OBSERVATEURS");
+            String resObs = rs.getString("RESOBS");
+            String IEspece = rs.getString("IESPECE");
+            String[] observateurs_tab = observateurs.split(",");     
+            //String[] resObs_tab = resObs.split(",");
+            //int[] resObs_tab2 = Integer.parseInt(resObs_tab);
+            ArrayList<Observateur> observateurs_liste;
+            for (int i = 0; i < observateurs_tab.length-1; i++) {
+                observateurs_liste.add(observateurs_tab[i]);
             }
+            ObsBatracien obsBatracien = new ObsBatracien(id, date, heure, lieu, observateurs_liste, resObs_tab, IEspece);
+            liste_ObsBatricien.add(obsBatracien);
         }
     }
 }
