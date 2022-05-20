40c38ffb21619bae72cc9bb8f62cd6e0f043fa62
Exanc <bapt.gic.22@gmail.com>
2022-05-20 16:02:30 +0200

    tkt

--- src/modele/traitement/Table.java
+++ src/modele/traitement/Table.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public abstract class Table <T> {
     
     //Jsp a quoi ça sert
+    // C'est pour supprimer une ligne de la table avec sont id
     public abstract void deleteEntry (int id);
     // TODO: Quel était la quatriéme méthode demandé par le prof
 }
