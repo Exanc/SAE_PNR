962c400cfc63c6adf7f980adc0ce3f4f57c3f9bd
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2022-06-20 19:10:02 +0200

    POPUP GENERIC + EView complet

--- src/vue/EView.java
+++ src/vue/EView.java
@@ -6,7 +6,13 @@ public enum EView {
     MENU("src/vue/assets/ui/menu.fxml"),
     CONSULTATION("src/vue/assets/ui/consultation.fxml"),
     IMPORTATION("src/vue/assets/ui/importation.fxml"),
-    SAISIE("src/vue/assets/ui/saisie.fxml");
+    SAISIE("src/vue/assets/ui/saisie.fxml"),
+    ADMIN_CONSOLE("src/vue/assets/ui/admin_console.fxml"),
+    ADMIN_CONSULTATION("src/vue/assets/ui/admin_consultation.fxml"),
+    ADMIN_UTILISATEURS("src/vue/assets/ui/admin_utilisateurs.fxml"),
+    GENERIC_POPUP("src/vue/assets/ui/popup_generic.fxml"),
+    POPUP_PREFERENCES("src/vue/assets/ui/popup_preferences.fxml"),
+    POPUP_CHOUETTE("src/vue/assets/ui/popup_selection_chouette.fxml");
 
     private String fileName;
 
