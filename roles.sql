28831c9553f1c361a08fb59109df4f3344222506
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2022-06-22 20:19:31 +0200

    Admin utilisateur

--- roles.sql
+++ roles.sql
@@ -2,7 +2,9 @@ use pnr;
 
 DROP ROLE 'administrator';
 CREATE ROLE 'administrator';
-GRANT ALL ON pnr.* TO 'administrator';
+GRANT CREATE USER, GRANT OPTION, ROLE_ADMIN ON *.* TO 'administrator';
+GRANT SELECT ON mysql.role_edges TO 'administrator';
+GRANT ALL PRIVILEGES ON pnr.* TO 'administrator';
 
 DROP ROLE 'observer';
 CREATE ROLE 'observer';
@@ -17,6 +19,8 @@ CREATE USER 'francis'@'localhost' IDENTIFIED BY 'lol';
 GRANT 'administrator' to 'francis'@'localhost';
 
 SET DEFAULT ROLE ALL TO 'francis'@'localhost';
+
+FLUSH PRIVILEGES;
 
 Select max(idObs)
 from observation;
